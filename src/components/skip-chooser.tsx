84b819d5c22df0417a3542c7ac9d3d4d9874656d
Collins Kipkemoi <chemerilcollins@gmail.com>
2025-06-07 23:00:09 +0200

    Feat: Add Skip component rendering data from endpoint.

--- src/components/skip-chooser.tsx
+++ src/components/skip-chooser.tsx
@@ -1,11 +1,14 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import Skip from "@/components/skip"
+import type { SkipData } from "@/types"
 
 function SkipChooser() {
+    const [skips, setSkips] = useState<SkipData[]>([])
     useEffect(() => {
         async function fetchData() {
             const response = await fetch("https://app.wewantwaste.co.uk/api/skips/by-location?postcode=NR32&area=Lowestoft")
             const data = await response.json()
-            console.log(data)
+            setSkips(data)
         }
         fetchData()
     }, [])
@@ -13,7 +16,11 @@ function SkipChooser() {
         <div className="flex flex-col items-center justify-center w-full pt-10">
             <h1 className="text-2xl font-bold text-muted-foreground">Skip Chooser</h1>
             <p className="text-muted-foreground">Select the skip size that best suits your needs</p>
-
+            <div className="flex flex-wrap gap-4 w-full pt-10">
+                {skips.map((skip) => (
+                    <Skip key={skip.id} skip={skip} />
+                ))}
+            </div>
         </div>
     )
 }
